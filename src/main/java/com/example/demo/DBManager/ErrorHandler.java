603474fb7f4feaec7d28a29ac291f79bc3a0f06f
Mathilde <math_dahl@hotmail.com>
2020-10-21 13:58:49 +0200

    idk at this point

--- src/main/java/com/example/demo/DBManager/ErrorHandler.java
+++ src/main/java/com/example/demo/DBManager/ErrorHandler.java
@@ -3,14 +3,14 @@ package com.example.demo.DBManager;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ErrorHandler implements ErrorController {
 
-    @RequestMapping("/error")
+    @GetMapping("/error")
     public String error(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
